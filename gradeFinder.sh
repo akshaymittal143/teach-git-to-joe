ec37c0c72b33ba2ef02e5e6a75d5c2503825623a
Akshay Mittal <akshaycanodia@gmail.com>
2018-11-17 17:55:09 -0500

    fix space in if stmt

--- gradeFinder.sh
+++ gradeFinder.sh
@@ -5,13 +5,13 @@ if [ $marks -ge 90 ]
 then
   echo "You got A"
   
-elif [$marks -ge 80 ]
+elif [ $marks -ge 80 ]
 then
   echo "You got B"
-elif [$marks -ge 70 ]
+elif [ $marks -ge 70 ]
 then
   echo "You got C"
-elif [$marks -ge 60 ]
+elif [ $marks -ge 60 ]
 then
   echo "You got D"
 else
